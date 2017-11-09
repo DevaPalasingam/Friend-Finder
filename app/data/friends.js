573e3dde64d7a1bfd71994baa157f5d1a8348533
Deva <deva.palasingam@gmail.com>
2017-11-09 17:27:35 -0600

    long way to go

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friendsArray = [
+	{
+	  	name:"Gimli",
+	  	photo:"https://upload.wikimedia.org/wikipedia/en/f/f0/Gimli_With_Axe.jpg",
+	  	scores: [
+	      3,
+	      1,
+	      2,
+	      1,
+	      2,
+	      4,
+	      1,
+	      1,
+	      5,
+	      1
+	    ]
+	},
+	{
+	  	name:"Pippen",
+	  	photo:"http://images2.fanpop.com/image/photos/12300000/Pippin-Took-pippin-took-12389182-960-406.jpg",
+	  	scores: [
+	      4,
+	      4,
+	      3,
+	      4,
+	      3,
+	      5,
+	      1,
+	      3,
+	      3,
+	      1
+	    ]
+	},
+	{
+	  	name:"Gandalf",
+	  	photo:"https://wallpaperclicker.com/storage/wallpaper/gandalf-the-lord-of-the-rings-86888456.jpg",
+	  	scores: [
+	      5,
+	      5,
+	      4,
+	      5,
+	      1,
+	      3,
+	      3,
+	      3,
+	      3,
+	      3
+	    ]
+	}
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendsArray;
