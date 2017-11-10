d0ab62c43d4e12c4d3a0fb45f8a77bf955483a0c
Deva <deva.palasingam@gmail.com>
2017-11-10 00:28:18 -0600

    much testing to do

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,12 @@
+var friends = require("../data/friends");
+
+
+module.exports = function(app) {
+
+	// when user visits localhost:PORT/api/friendsData... they get a JSON of the friends in the database
+	app.get("/api/friendsData", function(req, res) {
+    	res.json(friends);
+  	});
+
+
+}
--- server.js
+++ server.js
@@ -1,3 +1,24 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
